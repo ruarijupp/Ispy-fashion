057445bafb6dc00143f3b023dfe884f8cbb2ee04
Ruari Jupp <ruarijupp@gmail.com>
2025-07-25 15:57:27 +0200

    Fix vite.config.js to valid config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-cat > vite.config.js <<EOF
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,4 +10,3 @@ export default defineConfig({
     emptyOutDir: true
   }
 });
-EOF
